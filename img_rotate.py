ffe63c236dcf3ebb8c8d156f3c99e4f1bac8fbe1
Kyle Fox <kyle@Mercury.local>
2009-04-02 22:47:26 -0600

    first commit

---
+++ img_rotate.py
@@ -0,0 +1,61 @@
+import Image, ImageFile
+
+# PIL's Error "Suspension not allowed here" work around:
+# s. http://mail.python.org/pipermail/image-sig/1999-August/000816.html
+ImageFile.MAXBLOCK = 1024*1024
+
+# The EXIF tag that holds orientation data.
+EXIF_ORIENTATION_TAG = 274
+
+# Obviously the only ones to process are 3, 6 and 8.
+# All are documented here for thoroughness.
+ORIENTATIONS = {
+    1: ("Normal", 0),
+    2: ("Mirrored left-to-right", 0),
+    3: ("Rotated 180 degrees", 180),
+    4: ("Mirrored top-to-bottom", 0),
+    5: ("Mirrored along top-left diagonal", 0),
+    6: ("Rotated 90 degrees", -90),
+    7: ("Mirrored along top-right diagonal", 0),
+    8: ("Rotated 270 degrees", -270)
+}
+
+def fix_orientation(img, save_over=False):
+    """
+    `img` can be an Image instance or a path to an image file.
+    `save_over` indicates if the origina image file should be replaced by the new image.
+    * Note: `save_over` is only valid if `img` is a file path.
+    """
+    path = None
+    if not isinstance(img, Image.Image):
+        path = img
+        img = Image.open(path)
+    elif save_over:
+        raise ValueError("You can't use `save_over` when passing an Image instance.  Use a file path instead.")
+    try:
+        orientation = img._getexif()[EXIF_ORIENTATION_TAG]
+    except TypeError:
+        raise ValueError("Image file has no EXIF data.")
+    if orientation in [3,6,8]:
+        degrees = ORIENTATIONS[orientation][1]
+        img = img.rotate(degrees)
+        if save_over and path is not None:
+            try:
+                img.save(path, quality=95, optimize=1)
+            except IOError:
+                # Try again, without optimization (PIL can't optimize an image
+                # larger than ImageFile.MAXBLOCK, which is 64k by default).
+                # Setting ImageFile.MAXBLOCK should fix this....but who knows.
+                img.save(path, quality=95)
+        return (img, degrees)
+    else:
+        return (img, 0)
+
+if __name__ == "__main__":
+    import sys
+    path = sys.argv[1]
+    try:
+        image, degrees = fix_orientation(path, save_over=True)
+        print '%s was rotated %s degrees.' % (path, degrees)
+    except Exception, e:
+        print "** Error: %s" % e
